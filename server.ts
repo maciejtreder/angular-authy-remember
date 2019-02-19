839aa248e775ed07f58110a015f2e004c77d6017
Maciej Treder <contact@maciejtreder.com>
2019-02-19 12:18:41 +0100

    step 1

--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ app.set('views', './dist/browser');
 app.post('/auth/login', (req, res) => {
  if (req.body.login === 'foo' && req.body.password === 'bar') {
    authy.send_approval_request('Authy ID', {
-       message: 'Request to login to Angular two factor authentication with Twilio'
+       message: 'Log into your Angular and Authy application'
      }, null, null,  function(err, authResponse) {
        if (err) {
          res.status(400).send('Bad Request');
@@ -52,20 +52,27 @@ app.post('/auth/login', (req, res) => {
 });
  
 app.get('/auth/status', (req, res) => {
- authy.check_approval_status(req.headers.token, (err, authResponse) => {
-   if (err) {
-     res.status(400).send('Bad Request.');
-   } else {
-     if (authResponse.approval_request.status === 'approved') {
-       res.cookie('authentication', 'super-encrypted-value-indicating-that-user-is-authenticated!', {
-         maxAge: 5 * 60 * 60 * 60,
-         httpOnly: true
-       });
-     }
-     res.status(200).send({status: authResponse.approval_request.status});
-   }
- });
+  authy.check_approval_status(req.headers.token, (err, authResponse) => {
+    if (err) {
+      res.status(400).send('Bad Request.');
+    } else {
+      if (authResponse.approval_request.status === 'approved') {
+        res.cookie('authentication', 'super-encrypted-value-indicating-that-user-is-authenticated!', {
+          maxAge: 60 * 60,
+          httpOnly: true
+        });
+        if (req.headers.remember === 'true') {
+         res.cookie('remember', authResponse.approval_request._authy_id, {
+           maxAge: 5 * 60 * 60,
+           httpOnly: true
+         });
+       }
+      }
+      res.status(200).send({status: authResponse.approval_request.status});
+    }
+  });
 });
+  
  
 app.get('/auth/isLogged', (req, res) => {
  res.status(200).send({authenticated: req.cookies.authentication === 'super-encrypted-value-indicating-that-user-is-authenticated!'});
